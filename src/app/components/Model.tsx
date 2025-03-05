b000050d481e42128d38d49f41b2d8ddd3a519ef
faridmsta <faridmustafyev@gmail.com>
2025-03-05 23:18:57 +0400

    Miqat eliyir

--- src/app/components/Model.tsx
+++ src/app/components/Model.tsx
@@ -1,8 +1,11 @@
 'use client'
 import * as THREE from 'three'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
+import { useScroll } from 'motion/react'
+import { useFrame } from '@react-three/fiber'
+import { IntervalHistogram } from 'node:perf_hooks'
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -33,6 +36,43 @@ type GLTFResult = GLTF & {
 
 export function Model(props: React.JSX.IntrinsicElements['group']) {
   const { nodes, materials } = useGLTF('/silva/scene.gltf') as GLTFResult
+  const { scrollYProgress } = useScroll();
+
+  const [light, setLight] = useState(false)
+  const intervalId = useRef<null | NodeJS.Timeout >(null); // Use a ref to store the interval ID
+
+  const miqat = () => {
+    intervalId.current = setInterval(() => {
+      setLight((prev) => !prev);
+    }, 200);
+  };
+
+
+
+  useEffect(() => {
+    // Cleanup the interval when the component unmounts
+    return () => {
+      if (intervalId.current) {
+        clearInterval(intervalId.current);
+      }
+    };
+  }, []);
+
+  useFrame(() => {
+    if (scrollYProgress.get() > 0.95) {
+      // Only start the interval if it hasn't been started yet
+      if (!intervalId.current) {
+        miqat(); // Start the interval and store the ID
+      }
+    } else {
+      setLight(false);
+      if (intervalId.current) {
+        clearInterval(intervalId.current); // Clear the interval when scroll progress is less than 0.95
+        intervalId.current = null;
+      }
+    }
+  });
+
   return (
     <group {...props} dispose={null}>
       <group scale={0.01}>
@@ -84,22 +124,25 @@ export function Model(props: React.JSX.IntrinsicElements['group']) {
             geometry={nodes.s14_Material004_0.geometry}
             material={materials['Material.004']}
           >
-            <meshStandardMaterial
-              emissive={"#aaaa55"}
-              emissiveIntensity={5}
-              color={"#ffffff"}
-              toneMapped={false}
-            />
-
-            <spotLight
-              color={"#ffffaa"}
-              intensity={100}
-              distance={100}
-              angle={1}
-              penumbra={10}
-              position={[0, -8, 1]}
-              target-position={[0, 0, 0]}
-            />
+            {light && (
+              <meshStandardMaterial
+                emissive={"#aaaa55"}
+                emissiveIntensity={3}
+                color={"#ffffff"}
+                toneMapped={false}
+              />
+            )}
+            {light && (
+              <spotLight
+                color={"#ffffaa"}
+                intensity={100}
+                distance={100}
+                angle={1}
+                penumbra={10}
+                position={[0, -8, 1]}
+                target-position={[0, 0, 0]}
+              />
+            )}
           </mesh>
           <mesh
             castShadow
